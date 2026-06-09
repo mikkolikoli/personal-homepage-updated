77eda625cb411ebb0e480be0fc822bc9de914be1
Mikko Rajakorpi <mikko@mikkorajakorpi.fi>
2026-06-09 11:15:27 +0300

    CV and portfolio boilerplate

--- src/app/[locale]/cv/page.tsx
+++ src/app/[locale]/cv/page.tsx
@@ -1,10 +1,35 @@
 import { useTranslations } from "next-intl"
-import { ReactElement } from "react";
+import { ReactElement } from "react"
+import Image from "next/image"
 
 export default function Page(): ReactElement {
   const t = useTranslations('cv')
 
   return (
-    <div>CV</div>
+    <article className="flex flex-col md:flex-row items-center p-8">
+      <section className="flex flex-col">
+        <div className="avatar">
+          <div className="mask mask-circle w-3xs shadow-lg">
+            <Image
+              src="/portrait-500p.jpg"
+              width={500}
+              height={667}
+              alt="Portrait of Mikko Rajakorpi in front of colorful background"
+            />
+          </div>
+        </div>
+
+        <section className="flex flex-col prose pt-8">
+          <h1>Mikko Rajakorpi</h1>
+
+          <section>
+            <h2>Contact</h2>
+
+          </section>
+        </section>
+      </section>
+
+      <section></section>
+    </article>
   )
 }
--- src/app/[locale]/portfolio/page.tsx
+++ src/app/[locale]/portfolio/page.tsx
@@ -5,6 +5,15 @@ export default function Page(): ReactElement {
   const t = useTranslations('portfolio')
 
   return (
-    <div>Portfolio</div>
+    <div className="flex flex-col items-center p-8">
+      <h1 className="text-4xl font-bold p-8">Portfolio</h1>
+      <article className="card bg-base-200 w-96 shaodow-sm">
+        <figure></figure>
+
+        <section className="card-body">
+          <h2 className="card-title">Personal homepage</h2>
+        </section>
+      </article>
+    </div>
   )
 }
